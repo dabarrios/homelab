3d05295d52b404b21fb48820b9e5c04f0ff905ff
David Barrios <dabarrios@protonmail.com>
2026-06-08 11:50:33 -0700

    Refactor GameServer model and templates; add allocated_memory field, update verbose names, and improve detail views

--- homelab/game_servers/models.py
+++ homelab/game_servers/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class GameServer(models.Model):
-    game = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
-    version = models.CharField(max_length=255, blank=True)
-    port = models.PositiveIntegerField(null=True, blank=True)
-    is_active = models.BooleanField(default=False)
-    notes = models.TextField(blank=True)
+    game = models.CharField(max_length=255, db_column='GAME', verbose_name="Game")
+    name = models.CharField(max_length=255, db_column='NAME', verbose_name="Name")
+    slug = models.SlugField(unique=True, db_column='SLUG', verbose_name="Slug")
+    allocated_memory = models.CharField(max_length=255, db_column='ALLOCATED_MEMORY', verbose_name="Allocated Memory")
+    version = models.CharField(max_length=255, blank=True, db_column='VERSION', verbose_name="Version")
+    port = models.PositiveIntegerField(null=True, blank=True, db_column='PORT', verbose_name="Port")
+    is_active = models.BooleanField(default=False, db_column='IS_ACTIVE', verbose_name="Is Active?")
+    notes = models.TextField(blank=True, db_column='NOTES', verbose_name="Notes")
 
     # Controls how GameServer objects are displayed by using the name field as its string representation.
     def __str__(self):
@@ -16,5 +17,5 @@ class GameServer(models.Model):
     class Meta:
         # Human-readable singular name. Django uses it for: Add game server, Change game server, etc.
         verbose_name = 'game server'
-        # Human-readable plural name. Django uses it for the label in the admin interface.
+        # Human-readable plural name. Django uses it for the label in left-side panel under the header. "Game servers + Add"
         verbose_name_plural = 'game servers'
