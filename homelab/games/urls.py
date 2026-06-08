e32b7347396ad2605644cdcc199623ee81d95bcd
David Barrios <dabarrios@protonmail.com>
2026-06-08 02:38:03 +0000

    Added shared base.html page for other pages.

--- homelab/games/urls.py
+++ homelab/games/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('status/', views.game_status)
+    path('status/', views.servers_overview, name='servers_overview'),
+    path('status/<str:slug>/', views.server_details, name='server_details'),
 ]
