27cc1514f755194f9ae34591f41f89ef1d001b04
David Barrios <dabarrios@protonmail.com>
2026-06-08 14:02:57 -0700

    Make GameServer name field unique; remove data.py and update migrations

--- homelab/game_servers/views.py
+++ homelab/game_servers/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .data import get_server_list, get_server_details
 from .models import GameServer
 
 def game_server_list(request):
     servers = GameServer.objects.all()
-
     return render(request, 'game_server_list.html', {'servers': servers})
 
 def game_server_detail(request, slug):
@@ -13,5 +11,4 @@ def game_server_detail(request, slug):
         server = GameServer.objects.get(slug=slug)
     except GameServer.DoesNotExist:
         return HttpResponse('Server not found', status=404)
-
     return render(request, 'game_server_detail.html', {'server': server})
