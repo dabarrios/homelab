d7bb7442899b967dcae7db982d4763e7254e5c69
David Barrios <dabarrios@protonmail.com>
2026-06-08 11:50:33 -0700

    Refactor GameServer model and templates; add allocated_memory field, update verbose names, and improve detail views

--- homelab/game_servers/apps.py
+++ homelab/game_servers/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
 
 
-class GameServersConfig(AppConfig):
+class GameServerConfig(AppConfig):
     # Django's internal name for the app
     name = 'game_servers' 
-    # Human-readable name for the app, used in the admin interface
+    # Human-readable name for the app, used in the admin interface in the left-side panel header
     verbose_name = 'Game Servers'
--- homelab/game_servers/views.py
+++ homelab/game_servers/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .data import get_server_list, get_server_details
+from .models import GameServer
 
 def game_server_list(request):
-    servers = get_server_list()
+    servers = GameServer.objects.all()
 
     return render(request, 'game_server_list.html', {'servers': servers})
 
 def game_server_detail(request, slug):
-    server = get_server_details(slug)
-
-    if not server:
+    try:
+        server = GameServer.objects.get(slug=slug)
+    except GameServer.DoesNotExist:
         return HttpResponse('Server not found', status=404)
 
     return render(request, 'game_server_detail.html', {'server': server})
