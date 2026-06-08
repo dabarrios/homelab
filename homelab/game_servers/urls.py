00295fca917bebbae45d6d3b165d4be1b7a53ae6
David Barrios <dabarrios@protonmail.com>
2026-06-08 08:51:39 -0700

    Refactor game server templates and views; rename and create new HTML files for better clarity

--- homelab/game_servers/urls.py
+++ homelab/game_servers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('status/', views.servers_overview, name='servers_overview'),
-    path('status/<str:slug>/', views.server_details, name='server_details'),
+    path('overview/', views.game_server_list, name='game_server_list'),
+    path('<str:slug>/', views.game_server_detail, name='game_server_detail'),
 ]
