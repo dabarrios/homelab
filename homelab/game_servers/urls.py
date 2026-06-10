6b4723b7ea410a95b59cd47858a61e0357ad35b4
David Barrios <dabarrios@protonmail.com>
2026-06-10 11:23:13 -0700

    Refactor button styles and update URL path for game server edit notes: enhance button appearance with improved padding, background color, and hover effects; correct URL path from 'notes' to 'edit' for consistency.

--- homelab/game_servers/urls.py
+++ homelab/game_servers/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('overview/', views.game_server_list, name='game_server_list'),
     path('details/<str:slug>/', views.game_server_detail, name='game_server_detail'),
-    path('notes/<str:slug>', views.game_server_edit_notes, name='game_server_edit_notes'),
+    path('edit/<str:slug>', views.game_server_edit_notes, name='game_server_edit_notes'),
 ]
