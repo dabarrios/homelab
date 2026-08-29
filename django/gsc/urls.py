ac5636e1415e88375d8ad164de520eb6c04d415c
David Barrios <dabarrios@protonmail.com>
2026-08-28 18:29:02 -0700

    Set up PALS app with initial views and templates; update URL routing

--- django/gsc/urls.py
+++ django/gsc/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('details/<str:slug>/', views.game_server_detail, name='game_server_detail'),
     path('details/<str:slug>/power/', views.server_power, name='server_power'),
 ]
--- django/pals/views.py
+++ django/pals/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.db.models import Sum
 
-# Create your views here.
+def home(request):
+    # Placeholder for the home view
+    return render(request, 'base.html', {})
